ccca9f1327509a60d91e7995b53efc321b8f99bd
주동호 <100278301+JOODON@users.noreply.github.com>
2023-01-09 13:32:03 +0900

    다시시작

---
+++ src/main/java/com/example/aws_springboot/Web/HelloController.java
@@ -0,0 +1,6 @@
+package com.example.aws_springboot.Web;
+
+import org.springframework.stereotype.Controller;
+
+public class HelloController {
+}
---
+++ src/test/java/com/example/aws_springboot/AwsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
